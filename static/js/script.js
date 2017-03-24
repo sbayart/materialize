98f205bae4a2fbe6fb739750b11977647b8130ac
solene <solenantoine@gmail.com>
2017-03-24 15:26:39 +0100

    add page de présentation

--- static/js/script.js
+++ static/js/script.js
@@ -1,29 +1,44 @@
-$(document).ready(function(){
-  $('.carousel').carousel();
-$(".button-collapse").sideNav();
-$('.chips').material_chip();
-$('.chips-initial').material_chip({
-data: [{
- tag: 'Apple',
-}, {
- tag: 'Microsoft',
-}, {
- tag: 'Google',
-}],
-});
-$('.chips-placeholder').material_chip({
-placeholder: 'Enter a tag',
-secondaryPlaceholder: '+Tag',
-});
-$('.chips-autocomplete').material_chip({
-autocompleteOptions: {
- data: {
-   'Apple': null,
-   'Microsoft': null,
-   'Google': null
- },
- limit: Infinity,
- minLength: 1
-}
-});
+$(document).ready(function() {
+    $('.target').pushpin({
+        top: 0,
+        bottom: 1000,
+        offset: 0
+    });
+    $('.pushpin-demo-nav').each(function() {
+        var $this = $(this);
+        var $target = $('#' + $(this).attr('data-target'));
+        $this.pushpin({
+            top: $target.offset().top,
+            bottom: $target.offset().top + $target.outerHeight() - $this.height()
+        });
+    });
+    $('.carousel').carousel();
+    $(".button-collapse").sideNav();
+    $('.chips').material_chip();
+    $('.chips-initial').material_chip({
+        data: [{
+            tag: 'Apple',
+        }, {
+            tag: 'Microsoft',
+        }, {
+            tag: 'Google',
+        }],
+    });
+    $('.chips-placeholder').material_chip({
+        placeholder: 'Enter a tag',
+        secondaryPlaceholder: '+Tag',
+    });
+    $('.chips-autocomplete').material_chip({
+        autocompleteOptions: {
+            data: {
+                'Apple': null,
+                'Microsoft': null,
+                'Google': null
+            },
+            limit: Infinity,
+            minLength: 1
+        }
+    });
+      $('.parallax').parallax();
+        
 })
